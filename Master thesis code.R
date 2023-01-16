8d8d8152b13bd9cbd2d8353bc5f9cabf12e1b825
Ingridlett <122453525+Ingridlett@users.noreply.github.com>
2023-01-16 18:59:00 +0100

    Add files via upload
    Work done 16/01

--- Master thesis code.R
+++ Master thesis code.R
@@ -30,23 +30,35 @@ library(ggplot2)
 # names(konsum)
 # summary(konsum)
 
-write.table(handelstall, "C:/Users/lettr/OneDrive/Desktop/handelstall.xlxs", sep=",",row.names = FALSE)
-
-handelstall.filtrert <- read_excel("C:/lettr/OneDrive/Desktop/handelstall.v1.xlxs", 
-                            sheet = "Sheet 1")
 summary(handelstall)
 
-Pris <- mutate(handelstall=verdi_eur*mengde)
+klippfisk <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "klippfisk hel", date > "2010-12-01")
+saltet <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "saltet hel, konvensjonell", date > "2010-12-01")
+fryst <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "fryst hel", date > "2010-12-01")
 
-x1 <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "klippfisk hel", date > "2010-12-01")
-x2 <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "saltet hel, konvensjonell", date > "2010-12-01")
-x3 <- filter(handelstall, RAPPORT_LAND == "NO", FLYT == "E", Isocode=="PT", PRODUKTHOVEDNO == "fryst hel", date > "2010-12-01")
+### Klippfisk Time Series display ###
+### Looks to be forming peaks around year end each year (in line with tradition of bacalao in december),
+### however it is a little hard to tell, should try to display with every year and month displayed
+### portraying a common trend with a nonconstant mean?? The series are probably nonstationary.
+klippfisk <- ts(klippfisk, start = c(2010,12), end = c(2022, 11), frequency = 12)
+ts.plot(klippfisk[,"mengde"],klippfisk[,"Verdi"], type="l", lty=c(1,2), col=c(1,2), main="KLIPPFISK")
+legend("topleft", border=NULL, legend=c("mengde","Verdi"), lty=c(1,2), col=c(1,2))
 
-merge <- rbind(x1,x2,x3)
-merge <- merge %>% mutate(Pris = verdi_eur/mengde)
-merge$Pris
-summary(merge)
-names(merge)
+### Salted Fish Time Series display ###
+### Clear peaks showing seasonality around year end each year
+saltet <- ts(saltet, start = c(2010,12), end = c(2022, 11), frequency = 12)
+ts.plot(saltet[,"mengde"],saltet[,"Verdi"], type="l", lty=c(1,2), col=c(1,2), main="SALTET")
+legend("topleft", border=NULL, legend=c("mengde","Verdi"), lty=c(1,2), col=c(1,2))
+
+### Frozen Fish Time Series display ###
+### There are definite peaks but showing more variation than the other two time series. Very high peak
+### late 2021, perhaps around Christmas, compared to earlier years - What could it be due to? Perhaps
+### something to do with the pandemic (COVID-19) this would be interesting to delve into in terms of 
+### structural changes and our research question in that part of the paper.
+fryst <- ts(fryst, start = c(2010,12), end = c(2022, 11), frequency = 12)
+ts.plot(fryst[,"mengde"],fryst[,"Verdi"], type="l", lty=c(1,2), col=c(1,2), main="FRYST")
+legend("topleft", border=NULL, legend=c("mengde","Verdi"), lty=c(1,2), col=c(1,2))
+
 
 ??Plot
 
@@ -54,18 +66,57 @@ names(merge)
 ### example: Cod (whole) sold 2010-01-01 was amount 177.01 to a NOK value of 4000.00 meaning 
 ### we sold in January 2010 177 tonnes of cod to a value of MNOK 4,000,000 (4000*1000)
 
+library(tseries)
+
+# Test for stationarity,
+# H0: Unit-root, i.e., nonstationary.
+adf.test(klippfisk[,"mengde"])
+adf.test(klippfisk[,"Verdi"])
+
+adf.test(saltet[,"mengde"])
+adf.test(saltet[,"Verdi"])
+
+adf.test(fryst[,"mengde"])
+adf.test(fryst[,"Verdi"])
+
+# ADF in first differences
+adf.test(diff(klippfisk[,"mengde"]))
+adf.test(diff(klippfisk[,"Verdi"]))
+
+adf.test(diff(saltet[,"mengde"]))
+adf.test(diff(saltet[,"Verdi"]))
+
+adf.test(diff(fryst[,"mengde"]))
+adf.test(diff(fryst[,"Verdi"]))
+
+### The stationarity tests indicate that all three series are ...
+
+# check for cointegration
+# estimate the long-run relationship
+library(dynlm)
+fit1 <- dynlm(mengde~Verdi-1, data = klippfisk)
+summary(fit1)
+
+fit2 <- dynlm(mengde~Verdi-1, data = saltet)
+summary(fit2)
+
+fit3 <- dynlm(mengde~Verdi-1, data = fryst)
+summary(fit3)
+
+### the intercept term is omitted/not omitted bacause...
+### has or has no economic meaning
+
+# residuals
+ehat1_klipp <- resid(fit1)
+plot(ehat1_klipp)
+
+ehat2_salt <- resid(fit2)
+plot(ehat2_salt)
+
+ehat3_fryst <- resid(fit3)
+plot(ehat3_fryst)
+
+
 library("ggplot2")
 
-x.ts <- ts(merge$Pris, frequency = 12, start = c("2011"), end = ("2022"))
-plot(x.ts)
 
-x1 <- x1 %>% mutate(Pris=verdi_eur/mengde)
-x1ts <- x1$Pris, frequency()
-                    
-ts(merge$Pris, frequency = 12, start = c("2011"), end = ("2022"))
-plot(x.ts)
-
-y11 <- merge$Pris
-y12 <- merge$Pris
-
-plot(x.ts,y11,y12)
